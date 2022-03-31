190b05d28587a8b9508e9f280e6a3c4af4b73a5a
Daniel <soumenorgame@gmail.com>
2022-03-31 18:57:17 -0300

    add splash screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {Register} from './src/pages/Register';
 import {Home} from './src/pages/Home';
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC = () => {
   const Stack = createNativeStackNavigator();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <NavigationContainer>
       <Stack.Navigator
